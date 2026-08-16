c1ce2febe4ba5e9a47ed4a22c84bc5c300f0a980
Ben Stone <57852724+bstone100@users.noreply.github.com>
2026-08-15 23:26:22 -0700

    server is serving HTML page

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import { createServer } from "node:http";
+import { readFile } from "node:fs/promises";
 
 const server = createServer(async (req, res) => {
     if (req.method === "GET") {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end("Hello Ben, nice job on your server!");
-        return;
+        if (req.url === "/" || req.url === "/index.html") {
+            const data = await readFile("./index.html", "utf-8");
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+            return;
+        }
+        if (req.url === "/index.js") {
+            const data = await readFile("./index.js", "utf-8");
+            res.writeHead(200, {"Content-Type": "text/javascript"});
+            res.end(data);
+            return;
+        }
+        if (req.url === "/styles.css") {
+            const data = await readFile("./styles.css", "utf-8");
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.end(data);
+            return;
+        }
     }
 
     res.writeHead(404);
