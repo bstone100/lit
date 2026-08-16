dc93be9fe2062b53e2d14db413164696a72998da
Ben Stone <57852724+bstone100@users.noreply.github.com>
2026-08-15 23:48:17 -0700

    update test to check content of all files served

--- tests.js
+++ tests.js
@@ -1,10 +1,36 @@
 import { test } from "node:test";
 import assert from "node:assert/strict";
 
+const BASE_URL = "http://localhost:3000";
 
-test("Probe server", async () => {
-    const response = await fetch("http://localhost:3000/");
-    assert.equal(response.status, 200);
-    const data = await response.text();
-    assert.equal(data, "Hello Ben, nice job on your server!");
+test("Probe the page served by the server", async () => {
+
+    const checkFile = async (url, content) => {
+        const response = await fetch(`${BASE_URL}${url}`);
+        assert.equal(response.status, 200);
+        const data = await response.text();
+        assert.ok(data.includes(content));
+    }
+
+    // content is an excerpt from the file
+    const checks = [
+        {
+            url: "/",
+            content: "<title>Ben's Server</title>"
+        },
+        {
+            url: "/index.html",
+            content: "<title>Ben's Server</title>"
+        },
+        {
+            url: "/styles.css",
+            content: "color: red;"
+        },
+        {
+            url: "/index.js",
+            content: "document.querySelector"
+        }
+    ];
+
+    await Promise.all(checks.map(check => checkFile(check.url, check.content)));
 });
